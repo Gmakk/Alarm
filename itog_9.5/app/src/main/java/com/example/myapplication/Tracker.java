92fadf355bc5f2cf495685b5e37ae71a0e6b8930
Gmakk <71002826+Gmakk@users.noreply.github.com>
2020-10-25 17:02:43 +0300

    itog_10

---
+++ itog_9.5/app/src/main/java/com/example/myapplication/Tracker.java
@@ -0,0 +1,89 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+public class Tracker extends AppCompatActivity implements SensorEventListener
+{
+    SensorManager sensorManager;
+    TextView tv_Steps;
+
+    boolean isRunning = false;
+    float count  = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracker);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        tv_Steps = (TextView) findViewById(R.id.textView_Steps);
+
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            //ask for permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        isRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null)
+        {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+        else
+        {
+           // Toast.makeText(this, "STEP_COUNTER sensor not found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        isRunning = false;
+//    sensorManager.unregisterListener(this); // this will stop detecting steps!
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        if (isRunning)
+        {
+            tv_Steps.setText(String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+        // Needed this method to skip the error
+        // error: MainActivity is not abstract and does not override abstract method onAccuracyChanged(Sensor,int) in SensorEventListener
+        // public class MainActivity extends AppCompatActivity implements SensorEventListener
+    }
+
+    public void clickButton(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
